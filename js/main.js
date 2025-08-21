57978409cbb91038471619c98bc4e6c798e6ad61
Kledson Ferreira Fonseca <kledferr@gmail.com>
2025-08-21 11:51:13 -0300

    Ajuste no JS

--- js/main.js
+++ js/main.js
@@ -28,15 +28,13 @@ $(document).ready(function(){
             },
             sudmitHandler: function(form){
                 console.log(form)
+                alert(`Mensagem enviada com sucesso`)
             },
             invalidHandler: function(evento, validador){
                 let camposincorretos = validador.numberOfInvalids();
                 if (camposincorretos){
                     alert(`Existem ${camposincorretos} campos incorretos`)
-                } else {
-                        alert(`Mensagem enviada com sucesso`)
-                    }
-
                 }
+            }
         })
 })
